54272247b4e1ca2d22b37f640d31ee3cc1a0175a
Lasse_Meinen <lmeinen@ethz.ch>
2023-12-01 14:53:51 +0100

    check if endorsement contains required 'end' claim
    (cherry picked from commit f581eea0dd7acb87428853a7a4de2fa332cfa481)

--- pkg/tokens/claims.go
+++ pkg/tokens/claims.go
@@ -20,6 +20,7 @@ func init() {
 	jwt.RegisterCustomField("key", EmbeddedKey{})
 	jwt.RegisterCustomField("ass", []*ident.AI{})
 	jwt.RegisterCustomField("emb", EmblemConstraints{})
+	jwt.RegisterCustomField("ver", "")
 }
 
 var ErrIllegalConst = errors.New("json element is illegal constant")
@@ -162,6 +163,7 @@ func (ek *EmbeddedKey) UnmarshalJSON(bs []byte) (err error) {
 var ErrIllegalVersion = jwt.NewValidationError(errors.New("illegal version"))
 var ErrIllegalType = jwt.NewValidationError(errors.New("illegal claim type"))
 var ErrAssMissing = jwt.NewValidationError(errors.New("emblems require ass claim"))
+var ErrEndMissing = jwt.NewValidationError(errors.New("endorsements require end claim"))
 
 // Validation function for emblem tokens.
 var EmblemValidator = jwt.ValidatorFunc(func(_ context.Context, t jwt.Token) jwt.ValidationError {
@@ -188,6 +190,8 @@ var EndorsementValidator = jwt.ValidatorFunc(func(_ context.Context, t jwt.Token
 		if !check {
 			return ErrIllegalType
 		}
+	} else {
+		return ErrEndMissing
 	}
 
 	return nil
