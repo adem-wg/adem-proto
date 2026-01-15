4d6dc72b6b056a908f6bf2cde9d01e974212aee1
Felix Linker <linkerfelix@gmail.com>
2026-01-15 12:04:16 +0100

    Update emblem.felixlinker.de example

--- exm/emblem.felixlinker.de/sign.sh
+++ exm/emblem.felixlinker.de/sign.sh
@@ -4,17 +4,10 @@ go run github.com/adem-wg/adem-proto/cmd/emblemgen \
 
 go run github.com/adem-wg/adem-proto/cmd/emblemgen \
   -skey keys/emblem.felixlinker.de.pem -alg ES512 -proto protos/emblem.felixlinker.de.json \
-  -pk keys/emblem.pem -sign-kid -lifetime 31536000 > jws/emblem.felixlinker.de.jws
+  -logs certs/emblem.felixlinker.de.logs.json -pk keys/emblem.pem -lifetime 31536000 \
+  > jws/emblem.felixlinker.de.jws
 
 go run github.com/adem-wg/adem-proto/cmd/emblemgen \
   -skey keys/auth.felixlinker.de.pem -alg ES512 -proto protos/auth.felixlinker.de.json \
-  -pk keys/emblem.felixlinker.de.pem -sign-kid -lifetime 31536000 > jws/auth.felixlinker.de.jws
-
-go run github.com/adem-wg/adem-proto/cmd/kid -cmd encode \
-  -pk keys/auth.felixlinker.de.pem -pk-alg ES512 > jws/auth.felixlinker.de.pub
-
-go run github.com/adem-wg/adem-proto/cmd/kid -cmd encode \
-  -pk keys/emblem.felixlinker.de.pem -pk-alg ES512 > jws/emblem.felixlinker.de.pub
-
-go run github.com/adem-wg/adem-proto/cmd/kid -cmd encode \
-  -pk keys/emblem.pem -pk-alg ES512 > jws/emblem.pub
+  -logs certs/auth.felixlinker.de.logs.json -pk keys/emblem.felixlinker.de.pem -lifetime 31536000 \
+  > jws/auth.felixlinker.de.jws
