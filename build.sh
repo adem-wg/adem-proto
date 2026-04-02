5976501655cf34abf8761ca09093cdbf9740c103
Felix Linker <linkerfelix@gmail.com>
2026-04-02 09:51:51 +0200

    Build kid command

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 os=$(uname -s)
 arch=$(uname -m)
-for cmd in "ctcheck" "emblemcheck" "emblemgen" "leafhash" "probe" "records" "rootsetupcheck"; do
+for cmd in "ctcheck" "emblemcheck" "emblemgen" "kid" "leafhash" "probe" "records" "rootsetupcheck"; do
   go build -o "release/$cmd-$os-$arch" "github.com/adem-wg/adem-proto/cmd/$cmd"
 done
