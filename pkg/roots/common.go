63864e7c1265f682dc51622a554a1b944720d155
Felix Linker <linkerfelix@gmail.com>
2026-04-08 10:24:56 +0200

    More logical code structure

--- pkg/roots/common.go
+++ pkg/roots/common.go
@@ -2,12 +2,18 @@ package roots
 
 import (
 	"errors"
+	"fmt"
 	"log"
+	"net/url"
 
 	"github.com/adem-wg/adem-proto/pkg/tokens"
+	"github.com/adem-wg/adem-proto/pkg/util"
 	"github.com/lestrrat-go/jwx/v3/jwk"
 )
 
+var ErrIssNoHostName = errors.New("issuer has no hostname")
+var ErrCertNotForIss = errors.New("certificate is not valid for issuer OI")
+var ErrCertNotForKey = errors.New("certificate is not valid for key")
 var ErrNoLogConfig = errors.New("no log claim")
 
 type CTQueryResult struct {
@@ -25,6 +31,30 @@ func VerifyBindingCerts(iss string, key jwk.Key, logs []*tokens.LogConfig) []CTQ
 		verified[i].Ok = VerifyBinding(verified[i], iss, key) == nil
 	}
 	return verified
+}
+
+// Verify that the rootKey is correctly bound to the issuer OI in the
+// certificate's subjects referenced by the CT query.
+func VerifyBinding(q CTQueryResult, issuer string, rootKey jwk.Key) error {
+	kid, err := tokens.CalcKID(rootKey)
+	if err != nil {
+		log.Print("could not calculate KID")
+		return err
+	}
+	issuerUrl, err := url.Parse(issuer)
+	if err != nil {
+		log.Print("could not parse issuer")
+		return err
+	} else if issuerUrl.Hostname() == "" {
+		return ErrIssNoHostName
+	}
+
+	if !util.Contains(q.subjects, issuerUrl.Hostname()) {
+		return ErrCertNotForIss
+	} else if !util.Contains(q.subjects, fmt.Sprintf("%s.adem-configuration.%s", kid, issuerUrl.Hostname())) {
+		return ErrCertNotForKey
+	}
+	return nil
 }
 
 // Verify that the hashes in the log configs are included in the respective CT
--- pkg/roots/v1.go
+++ pkg/roots/v1.go
@@ -7,48 +7,16 @@ package roots
 import (
 	"context"
 	"errors"
-	"fmt"
 	"log"
-	"net/url"
 	"time"
 
-	"github.com/adem-wg/adem-proto/pkg/tokens"
-	"github.com/adem-wg/adem-proto/pkg/util"
 	"github.com/google/certificate-transparency-go/client"
 	"github.com/google/certificate-transparency-go/x509"
-	"github.com/lestrrat-go/jwx/v3/jwk"
 	"github.com/transparency-dev/merkle/proof"
 	"github.com/transparency-dev/merkle/rfc6962"
 )
 
-var ErrIssNoHostName = errors.New("issuer has no hostname")
-var ErrCertNotForIss = errors.New("certificate is not valid for issuer OI")
-var ErrCertNotForKey = errors.New("certificate is not valid for key")
 var ErrWrongEntryType = errors.New("do not recognize entry type")
-
-// Verify that the rootKey is correctly bound to the issuer OI in the
-// certificate's subjects referenced by the CT query.
-func VerifyBinding(q CTQueryResult, issuer string, rootKey jwk.Key) error {
-	kid, err := tokens.CalcKID(rootKey)
-	if err != nil {
-		log.Print("could not calculate KID")
-		return err
-	}
-	issuerUrl, err := url.Parse(issuer)
-	if err != nil {
-		log.Print("could not parse issuer")
-		return err
-	} else if issuerUrl.Hostname() == "" {
-		return ErrIssNoHostName
-	}
-
-	if !util.Contains(q.subjects, issuerUrl.Hostname()) {
-		return ErrCertNotForIss
-	} else if !util.Contains(q.subjects, fmt.Sprintf("%s.adem-configuration.%s", kid, issuerUrl.Hostname())) {
-		return ErrCertNotForKey
-	}
-	return nil
-}
 
 // Verify that the given certificate hash is included in the log identified by
 // the respective client.
